40ef68b01b0e9772d99da2852e1c7e3ee026939b
ucabbec <ucabbec@ucl.ac.uk>
2018-12-03 20:43:39 +0000

    match data set up as JS object

---
+++ src/match_data/commentaryData.tsx
@@ -0,0 +1,377 @@
+export default {
+    commentary: [
+        { 
+            index: 1,
+            time: "FT",
+            text: "It's over! Unai Emery's first north London derby ends in glorious victory! Aubameyang gave Arsenal the lead from the spot before Spurs surged into a half-time lead thanks to Dier and Kane's penalty. But Arsenal scored three times after the break through Aubameyang, Lacazette and Torreira before Vertonghen saw red for the away team. That was one of the games of the season... over to you Merseyside!!!"
+        },
+        {
+            index: 2,
+            time: "90+6",
+            text: "90+6: Foyth almost gives the ball away but Guendouzi can't take advantage. Wonder if Poch regrets starting the youngster ahead of Alderweireld?"
+        },
+        {
+            index: 3,
+            time: "90+4",
+            text: "There won't be a Tyson Fury-style resurrection from the canvas for Spurs, it seems. Time is drifting by and Arsenal are seeing it out."
+        },
+        {
+            index: 4,
+            time: "90+2",
+            text: "Arsenal fans are cheering every pass now. Tottenham fans are leaving the stadium. This is agony for the men in white."
+        },
+        {
+            index: 5,
+            time: "90+1",
+            text: "SIX added minutes will be added on. 'Arsenal fans will want it to be 16,' says Gary Neville who awards the Man Of the Match prize to Lucas Torreira."
+        },
+        {
+            index: 6,
+            time: "90",
+            text: "YELLOW! Aurier goes in late on Holding and another yellow card is produced."
+        },
+        {
+            index: 7,
+            time: "88",
+            text: "More pushing and shoving and Mike Dean has to step in as tempers flare again. The break just gives the Arsenal fans a chance to start another song. What a noise inside the Emirates!"
+        },
+        {
+            index: 8,
+            time: "87",
+            text: "Tottenham's last red card was on this day last year. It really isn'ty a good day for them."
+        },
+        {
+            index: 9,
+            time: "85",
+            text: "RED CARD! VERTONGHEN IS SENT OFF. It goes from bad to worse for Spurs. Vertonghen was booked for giving away the penalty for the opening goal and gets a second as he stretches for a ball, gets it, but his challenge takes down Lacazette on the ankle. Nasty."
+        },
+        {
+            index: 10,
+            time: "85",
+            text: "Sokratis tackles Eriksen 35 yards from goal and then celebrates as if he's scored the winner. Both fists pumping the air!"
+        },
+        {
+            index: 11,
+            time: "83",
+            text: "SUB! A final change for Spurs as Rose comes on to replaces Davies on the left."
+        },
+        {
+            index: 12,
+            time: "81",
+            text: "Sokratis lifts the ball over the bar, but this stadium is still bouncing. Arsenal's second-half form this season has been sensational."
+        },
+        {
+            index:13,
+            time: "79",
+            text: "DOUBLE SUB! Moura for Son and Winks for Alli as Pochettino gets desperate."
+        },
+        {
+            index: 14,
+            time: "77",
+            text: "GOAL: 'It's shaking. This stadium is shaking,' says Gary Neville. Aubameyang is the architect with a lovely through ball. Torreira gets past Dier and from an angle whips the ball across Lloris and into the far corner to make it 4-2."
+        },
+        {
+            index: 15,
+            time: "75",
+            text: "GOAL: Oh Foyth... what have you done? The Spurs defender gives the ball away to Ramsey on the halfway line and he quickly plays in Lacazette, who stumbles on the edge of the area before cutting back inside. He slips as he shoots and the ball deflects off Dier and past Lloris. The Emirates goes mad as Arsenal players pile on top of each other."
+        },
+        {
+            index: 16,
+            time: "74",
+            text: "The ball is cleared and Spurs break. A long ball sees Sissoko through - and Leno is off his line to clear."
+        },
+        {
+            index: 17,
+            time: "73",
+            text: "Lacazette urges the crowd to turn up the volume as he wins a corner off Davies. Both central defenders are up for this."
+        },
+        {
+            index: 18,
+            time: "72",
+            text: "That change sees Arsenal switch to a back four as the tactics are shuffled once more..."
+        },
+        {
+            index: 19,
+            time: "71",
+            text: "SUB! Mustafi can't continue. Guendouzi comes on and Arsenal have used up all their subs."
+        },
+        {
+            index: 20,
+            time: "69",
+            text: "A short break in play as Mustafi receives treatment after a collision. It looks as though Guendouzi is set to come on."
+        },
+        {
+            inde: 21,
+            time: "68",
+            text: "SO SLOPPY! Bellerin holds his hands up after almost costing his side a goal. He presents the ball to Son in a central area 20 yards from goal. The Spurs man shoots and Leno flings himself to his right to push the ball away and spare his team-mate's blushes."
+        },
+        {
+            index: 22,
+            time: '67',
+            text: "Lacazette crosses and Foyth does well to get there ahead of Aubameyang and Arsenal have a corner, which is promptly cleared."
+        },
+        {
+            index: 23,
+            time: '66',
+            text: "'Dier has gone into a back three to look after Ramsey Pochettino has changed it,' says Gary Neville as he spots a tactical switch from Spurs."
+        },
+        {
+            index: 24,
+            time: '65',
+            text: "YELLOW! Alli goes into the book for tripping Xhaka as the Arsenal man looked to break from midfield."
+        },
+        {
+            index: 25,
+            time: '63',
+            text: "63: WIDE! Good work from Lacazette before Kolasinac works the ball to Ramsey. It's a tight angle, but the shot is poor and flashes into the side-netting."
+        },
+        {
+            index: 26,
+            time: '62',
+            text: "Just over the bar from Sokratis! The defender leaps to reach a corner, but it's over Lloris' crossbar. Arsenal back on top. Can they make their dominance pay?"
+        },
+        {
+            index: 27,
+            time: '60',
+            text: "OFF THE LINE!!!!! The corner breaks to Mustafi and his shot looks destined for the back of the net - but Alli is there to clear it off the line. The ball lands at the feet of Torreira on the edge of the arae and he smashes it narrowly wide. So close!"
+        },
+        {
+            index: 28,
+            time: '59',
+            text: "CORNER! Aubameyang retrieves a loose ball after a Xhaka cross and earns a corner off Dier. The noise goes up another level."
+        },
+        {
+            index: 29,
+            time: '58',
+            text: "58: Superb stuff from Aubameyang. Could he become only the fourth man to score a hat-trick in a north London derby? Arsenal are right back in this and their fans are beckoning them forward."
+        },
+        {
+            index: 30,
+            time: '56',
+            text: "Goal: A superb finish from the Arsenal man from the edge of the area and this game is level! Ramsey does brilliantly to run onto a through ball and passes first time t'o 'Aubameyang through two defenders. The striker hits it first time a'nd' curls it beyond Lloris. Great goal. Game on!"
+        },
+        {
+            index: 31,
+            time: '55',
+            text: "Ramsey sees a shot blocked before Torreira stumbles in the area. The home team keep the ball alive, but a good position is wasted when Holding opts to shoot from miles away. No need..."
+        },
+        {
+            index: 32,
+            time: '52',
+            text: "A replay of that last corner shows Kolasinac grabbing Foyth by the neck... risky!"
+        },
+        {
+            index: 33,
+            time: '51',
+            text: "Eriksen's corner is headed out to Vertonghen, but his weak effort is held by Leno."
+        },
+        {
+            index: 34,
+            time: '50',
+            text: "SAVE! Kane's free-kick is a good one from 35 yards. Leno dives to his left to push the ball over with his right hand."
+        },
+        {
+            index: 35,
+            time: '49',
+            text: "YELLOW! Another silly foul. Another good position for Spurs. Xhaka trips Alli and Arsenal continue to play with fire."
+        },
+        {
+            index: 36,
+            time: '48',
+            text: "Eriksen's delivery is brilliant. Leno flaps at, fairly miserably, it and Mustafi concedes the corner before Kane can get a head to it. The same defender heads away the danger again and Arsenal clear their lines."
+        },
+        {
+            index: 37,
+            time: '47',
+            text: "Spurs start the half on the offensive and earn a free-kick in a dangerous position as Alli is felled by Kolasinac. They scored from a similar situation in the first half..."
+        },
+        {
+            index: 38,
+            time: '46',
+            text: "DOUBLE SUB! No messing from Emery. Lacazette and Ramsey on. Iwobi and Mkhitaryan off. A change of system as Spurs kick things off."
+        },
+        {
+            index: 39,
+            time: '45+4',
+            text: "SAVE! Mustafi gets his head on it and his looping effort looks to be sneaking in... Lloris does really well to dive and claw it away from just under the bar."
+        },
+        {
+            index: 40,
+            time: '45+3',
+            text: "Aurier nicks the ball off Iwobi and Arsenal will have one final corner in this half..."
+        },
+        {
+            index: 41,
+            time: '45+1',
+            text: "THREE added minutes. We don't want this half to end!!"
+        },
+        {
+            index: 42,
+            time: '45+1',
+            text: "A corner is delivered and Aubameyang heads it back into the mixer. Mustafi tries an audacious over-head shot but it's high and wild and over."
+        },
+        {
+            index: 43,
+            time: '45',
+            text: "Dier goes to ground in the penalty area, but makes a brilliant sliding tackle on Kolasinac to concede a corner."
+        },
+        {
+            index: 44,
+            time: '43',
+            text: "SAVE!! Aubameyang should put the ball in the back of the net! Kolasinac crosses low from the left and the striker takes it first time b'ut' Lloris does well to block. The offside flag was up... so it wouldn't have counted anyway."
+        },
+        {
+            index: 45,
+            time: '41',
+            text: "Holding gives the ball away on the edge of the box and Alli is in - but his low pass is cut out by Sokratis. Sloppy from Arsenal."
+        },
+        {
+            index: 46,
+            time: '38',
+            text: "BOOKING WATCH! In all the chaos, Dier was booked for his goal celebration (putting his finger to his lips and running at the home fans) while Mustafi also picked up a yellow in the melee."
+        },
+        {
+            index: 47,
+            time: '36',
+            text: "Unbelievable scenes here. Holding's decision to dive in on Son was utterly foolish. He lost his head in the aftermath of Tottenham's equaliser. There was more pushing and shoving before the kick was taken, but Kane kept his cool."
+        },
+        {
+            index: 48,
+            time: '34',
+            text: "GOAl: Kane goes to Leno's right. The goalkeeper dives the wrong way and somehow Spurs are ahead in this game. What a turnaround!!!"
+        },
+        {
+            index: 49,
+            time: '33',
+            text: "PENALTY TO SPURS!!! Mike Dean points to the spot again. Holding is penalised for diving in on Son. It looks like a pretty clear decision on the replays."
+        },
+        {
+            index: 50,
+            time: '30',
+            text: "GOAL: Dier gets to Eriksen's free-kick at the front post and heads low past Leno. The goalkeeper looked to be behind it but couldn't keep it out. Spurs are level!!! The celebrations all get a bit unsavoury as Spurs celebrate in front of the home fans. A load of Arsenal players get involved and things take a few seconds to calm down."
+        },
+        {
+            index: 51,
+            time: '29',
+            text: "Sokratis trips the lively Son as the Spurs man looked to do some damage on the left. Tottenham have a free-kick in a dangerous position. Before it's taken, Mike Dean calls Eirc Dier over for a few words."
+        },
+        {
+            index: 52,
+            time: '27',
+            text: "Son has been Tottenham's best player. He runs at the Arsenal backline and Totrreira and Sokratis combine to pinch the ball off his feet. The ball breaks to Sissoko who wastes his opportunity. This is a fabulous game of football."
+        },
+        {
+            index: 53,
+            time: '25',
+            text: "Mustafi's back-pass is a bit firm and for a brief moment it looks like it will catch Leno out and go into his own net, but the goalkeeper is alert and gets back to control the bouncing ball."
+        },
+        {
+            index: 54,
+            time: '23',
+            text: "SHOT! Son beats a couple of players and gets into the area before driving a low shot that Leno has to get down and push away, before gathering at the second attempt. Spurs are finally getting a foothold."
+        },
+        {
+            index: 55,
+            time: '22',
+            text: "CORNER! For Spurs. A chance for them to do something, but Davies' cross doesn't beat the first man."
+        },
+        {
+            index: 56,
+            time: '21',
+            text: "Aubameyang is flagged offside and Gary Neville is frustrated on his behalf. 'I thought Aubameyang would play on his own. If Tottenham do play up the pitch, he mustn't go too early.'"
+        },
+        {
+            index: 57,
+            time: '19',
+            text: "SO CLOSE! Spurs switch off as the free-kick is floated to Kolasinac on the left. He feeds Iwobi whose goalbound effort is saved by the feet of Lloris. In the follow-up move, Bellerin sees a shot blocked and Torreira smashes a drilled effort wide. Arsenal are completely dominant."
+        },
+        {
+            index: 58,
+            time: '18',
+            text: "Davies floors Mkhitaryan and Arsenal can send another delivery towards the Arsenal box..."
+        },
+        {
+            index: 59,
+            time: '15',
+            text: "PHEW! Let's catch our breaths for a moment. It's been an explosive start from Arsenal. Spurs just haven't settled into this one and two rash errors from Aurier and then Vertonghen saw them fall behind."
+        },
+        {
+            index: 60,
+            time: '13',
+            text: "BIG CHANCE! Iwobi feeds Kolasinac and his low pass finds Aubameyang in the six-yard box. He just can't get his shot away and the ball bobbles off his foot. That was not far away!"
+        },
+        {
+            index: 61,
+            time: '12',
+            text: "SHOT! Son has Spurts' first chance of the game, driving from an angle and forcing Leno to beat it away and out for a corner, which is wasted."
+        },
+        {
+            index: 62,
+            time: '10',
+            text: "Goal: Lloris dives to his left and Aubameyang calmly slots it the other way to give the home team the lead to huge roars.'Fully deserved,' says Gary Neville. 'Madness from Spurs.'"
+        },
+        {
+            index: 63,
+            time: '7',
+            text: "Iwobi's shot is blocked and Foyth sees the ball out of play off Kolasinac for a goal kick. 'On the ball, Arsenal do really look assured,' says Gary Neville."
+        },
+        {
+            index: 64,
+            time: '6',
+            text: "Arsenal are dominating the ball in the early stages and Spurs only win it back when Bellerin's through pass goes astray. The home fans are in fine voice early on."
+        },
+        {
+            index: 65,
+            time: '3',
+            text: "Arsenal have started positively. Bellerin is the latest to send in a cross from the right and Foyth does well to nod clear."
+        },
+        {
+            index: 66,
+            time: '2',
+            text: "EARLY CHANCE! Mkhitaryan breaks down the right and crosses for Aubameyang. It's awkward for the Arsenal striker and his header is over the bar."
+        },
+        {
+            index: 67,
+            time: '1',
+            text: "KICK-OFF! Here we go! The first derby of the post-Wenger era. Arsenal get the game going..."
+        }
+    ],
+    keyEvents:[
+        {
+            event: "RED CARD: Jan Vertonghen",
+            time: 85,
+            linkedId: 9
+        },
+        {
+            event: "GOAL: Arsenal (Torreira)",
+            time: 77,
+            linkedId: 9
+        },
+        {
+            event: "GOAL: Arsenal (Lacazette)",
+            time: 74,
+            linkedId: 9
+        },
+        {
+            event: "GOAL: Arsenal (Aubameyang)",
+            time: 56,
+            linkedId: 9
+        },
+        {
+            event: "GOAL: Tottenham (Kane)",
+            time: 34,
+            linkedId: 10
+        },
+        {
+            event: "GOAL: Tottenham (Dier)",
+            time: 30,
+            linkedId: 10
+        },
+        {
+            event: "GOAL: Arsenal (Aubameyang)",
+            time: 10,
+            linkedId: 10
+        }
+    ]
+};
+
